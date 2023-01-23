0868aa273d242b1ec98caeeceb736218acba3d3f
Evgeniy <example@.com>
2023-01-24 02:52:22 +0300

    automated questions

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
+/*jshint esversion: 6 */
 "use strict";
+
 
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
@@ -10,11 +12,24 @@ const personalMovieDB = {
     privat: false
 };
 
-let answer1 = prompt('Один из последних просмотренных фильмов?', ''),
-    answer2 = +prompt('Насколько оцените его?', ''),
-    answer3 = prompt('Один из последних просмотренных фильмов?', ''),
-    answer4 = +prompt('Насколько оцените его?', '');
+if (personalMovieDB.count < 10) {
+    alert('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    alert('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+    alert ('Вы киноман');
+} else {
+    alert('Произошла ошибка');
+}
 
-personalMovieDB.movies[answer1] = answer2;
-personalMovieDB.movies[answer3] = answer4;
+for (let i = 0; i < 2; i++) {
+    let a = prompt('Один из последних просмотренных фильмов?', '');
+    let b = +prompt('Насколько оцените его?', '');
+    if (a != '' && b != '' && a != null && b != null && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+    } else {
+        i--;
+    }
+}
 console.log(personalMovieDB);
+
